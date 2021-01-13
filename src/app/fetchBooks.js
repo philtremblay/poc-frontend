9ccb153b5c83ddf101608d886095c885ac2e38a1
Philippe Tremblay <philippe@metrio.net>
2021-01-13 08:58:17 -0500

    Add console logs for debug

--- src/app/fetchBooks.js
+++ src/app/fetchBooks.js
@@ -4,6 +4,7 @@ import {fetchBooksPending, fetchBooksSuccess, fetchBooksError} from './action'
 function fetchBooks() {
     return dispatch => {
         dispatch(fetchBooksPending());
+        console.log("calling:", process.env.REACT_APP_BACKEND_API)
         axios.get(process.env.REACT_APP_BACKEND_API)
         .then(res => {
             dispatch(fetchBooksSuccess(res.data))
